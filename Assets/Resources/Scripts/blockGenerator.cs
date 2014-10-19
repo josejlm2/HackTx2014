3d91e275d3b3346a7edffca871eb4419f8d95186
Jose Manriquez <josejlm2@neo.tamu.edu>
2014-10-18 23:11:45 -0500

    Working Blocks

---
+++ Assets/Resources/Scripts/blockGenerator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class blockGenerator : MonoBehaviour {
+	
+	public int spawnRate = 1;
+	private float timeSinceLastSpawn = 0;
+	
+	public GameObject oneCube;
+	
+	// Use this for initialization
+	void Start () {
+		//Debug.Log("ran cube creator");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//timeSinceLastSpawn += Time.realtimeSinceStartup;
+		//Debug.Log("running cube creator" + timeSinceLastSpawn );
+		// if ( timeSinceLastSpawn > spawnRate )
+		//  {
+		//Clone the cubes and randomly place them
+		GameObject newCube = (GameObject)GameObject.Instantiate(oneCube);
+		
+		newCube.transform.position = new Vector3(0, 0, 20.0f);
+		
+		newCube.transform.Translate(Random.Range(-100, 100), Random.Range(0,100), 1.0f);
+		
+		timeSinceLastSpawn = 0;
+		//Debug.Log("cube created");
+		// }
+	}
+}
